8c281e7dde07c9dfbb88ef7c32f3b034877909c8
Johan Nilsson <yoohahn@gmail.com>
2022-08-19 16:11:57 +0200

    only call tibber api once every hour

--- tibber/get-price.go
+++ tibber/get-price.go
@@ -3,15 +3,25 @@ package tibber
 import (
 	"context"
 	"encoding/json"
+	"time"
 
 	"github.com/machinebox/graphql"
 )
 
+func getCurrentHour() int {
+	return time.Now().Hour()
+}
+
+var cachedHour = -1
+var cachedJson = ""
+
 // Returns the body as first paramater and if the request was succes or not
 func GetPrice() (string, bool) {
 	hasError := false
-	client := graphql.NewClient("https://api.tibber.com/v1-beta/gql")
-	req := graphql.NewRequest(`
+	result := cachedJson
+	if cachedHour != getCurrentHour() || cachedJson == "" {
+		client := graphql.NewClient("https://api.tibber.com/v1-beta/gql")
+		req := graphql.NewRequest(`
         {
             viewer {
                 homes {
@@ -35,22 +45,24 @@ func GetPrice() (string, bool) {
             }
         }
     `)
-	req.Header.Set("authorization", "Bearer "+GetMyApiKey())
-	req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("authorization", "Bearer "+GetMyApiKey())
+		req.Header.Set("Content-Type", "application/json")
 
-	var graphqlResponse interface{}
-	if err := client.Run(context.Background(), req, &graphqlResponse); err != nil {
-		panic(err)
-	}
+		var graphqlResponse interface{}
+		if err := client.Run(context.Background(), req, &graphqlResponse); err != nil {
+			panic(err)
+		}
 
-	responseAsJson, err := json.Marshal(graphqlResponse)
-	result := ""
+		responseAsJson, err := json.Marshal(graphqlResponse)
 
-	if err != nil {
-		result = err.Error()
-		hasError = true
-	} else {
-		result = string(responseAsJson)
+		if err != nil {
+			result = err.Error()
+			hasError = true
+		} else {
+			result = string(responseAsJson)
+			cachedJson = result
+			cachedHour = getCurrentHour()
+		}
 	}
 	return result, hasError
 }
